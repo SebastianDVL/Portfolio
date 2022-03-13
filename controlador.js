507b95ba15d3efb15c3ddb8aa76eb7b4985b8fee
Sebas <sebas.cay@gmail.com>
2022-03-13 00:11:42 -0500

    Se Completa Header, Se Implementa Dark Mode Y Se Organiza El Main

--- controlador.js
+++ controlador.js
@@ -1,1 +1,13 @@
 
+let select = document.querySelector('select')
+let html = document.querySelector('html')
+
+select.addEventListener('change',()=>{
+    if(select.value == 'dark'){
+        html.classList.add('dark')
+    }else{
+        html.classList.remove('dark')
+    }
+})
+
+
