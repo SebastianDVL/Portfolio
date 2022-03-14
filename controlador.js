ddc252298d10feb23ddad25cab41322d9d3000cd
Sebas <sebas.cay@gmail.com>
2022-03-13 23:10:03 -0500

    github api

--- controlador.js
+++ controlador.js
@@ -9,5 +9,25 @@ select.addEventListener('change',()=>{
         html.classList.remove('dark')
     }
 })
+function requestUserRepos(username){
+    
+   const xhr = new XMLHttpRequest();   
+    const url = `https://api.github.com/users/${username}/repos`;
+    
+    xhr.open('GET', url, true);
+    
+    xhr.onload = function() {
+    
+        const data = JSON.parse(this.response);
+        
+    
+    }
+    
+    // Send the request to the server
+    xhr.send();
+    
+}
 
+// Call function passing in 'facebook' as GitHub username
+requestUserRepos('SebastianDvl');
 
